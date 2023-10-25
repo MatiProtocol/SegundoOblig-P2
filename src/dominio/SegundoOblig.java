d554cd125a6cd3354759c52ea2480034230cf7e2
ValentinoBarreiro <goran.donna@gmail.com>
2023-10-25 20:07:18 -0300

    nose
    ola?

--- src/dominio/SegundoOblig.java
+++ src/dominio/SegundoOblig.java
@@ -14,7 +14,7 @@ public class SegundoOblig {
      * @param args the command line arguments
      */
     public static void main (String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here   erwerwerwerwoerweiurjweriwejriwe
     }
     
 }
