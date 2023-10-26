3adbef1c4417d7910f1bc94cb086dd2a59e5da62
ValentinoBarreiro <goran.donna@gmail.com>
2023-10-26 16:36:55 -0300

    erg
    ger

---
+++ src/interfaz/SegundoOblig.java
@@ -0,0 +1,15 @@
+package interfaz;
+
+import dominio.Postulante;
+
+
+public class SegundoOblig {
+
+
+    public static void main (String[] args) {
+       Principal v = new Principal();
+       v.setVisible(true);
+
+    }
+    
+}
