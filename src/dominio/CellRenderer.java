8d177d54e1b241ea02ddb0ae55c916e963464db8
ValentinoBarreiro <goran.donna@gmail.com>
2023-11-20 15:28:14 -0300

    nada
    borre un //

--- src/dominio/CellRenderer.java
+++ src/dominio/CellRenderer.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dominio;
 
 import javax.swing.table.*;
@@ -27,7 +23,6 @@ public class CellRenderer extends DefaultTableCellRenderer {
     
     private String getHTML(String string) {
         
-        //ArrayList<String> separacion = new Pintura(comentario);
         
             StringBuilder sb = new StringBuilder();
             sb.append("<html>");
